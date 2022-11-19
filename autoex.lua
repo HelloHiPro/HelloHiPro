966767d73d9bbf8f06c64c76ac021d4755c21c6e
HelloHiPro <118133915+HelloHiPro@users.noreply.github.com>
2022-11-19 15:18:25 +0000

    Update autoex.lua

--- autoex.lua
+++ autoex.lua
@@ -1,4 +1,4 @@
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport [[
-       repeat wait() until game:IsLoaded() wait(5) print("ServerHoped or rejoined")
-       loadstring(game:HttpGet('https://raw.githubusercontent.com/948265/ax/main/autoexec'))()
-       loadstring(game:HttpGet('https://script.karmapanda.dev'))()]]
+repeat wait() until game:IsLoaded() wait(5) print("ServerHoped or rejoined")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/786968e247b5628dceace97b0a891b0298166a0d/autoex.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/main/New_Text_Document.lua'))()]]
