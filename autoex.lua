786968e247b5628dceace97b0a891b0298166a0d
HelloHiPro <118133915+HelloHiPro@users.noreply.github.com>
2022-11-19 15:15:37 +0000

    Add files via upload

---
+++ autoex.lua
@@ -0,0 +1,4 @@
+local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport [[
+       repeat wait() until game:IsLoaded() wait(5) print("ServerHoped or rejoined")
+       loadstring(game:HttpGet('https://raw.githubusercontent.com/948265/ax/main/autoexec'))()
+       loadstring(game:HttpGet('https://script.karmapanda.dev'))()]]
