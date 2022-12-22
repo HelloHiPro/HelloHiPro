38570bd9d68bad8ff40cc53c23367ca6498df818
948265 <111317527+948265@users.noreply.github.com>
2022-12-22 17:27:12 -0300

    Update New_Text_Document.lua

--- New_Text_Document.lua
+++ New_Text_Document.lua
@@ -2400,7 +2400,7 @@ SettingsTab:AddToggle({
         if _G.SettingsTable.autoexecute and autoex == 0 then autoex = autoex + 1
         local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport [[
         
-repeat wait until game:IsLoaded()
+repeat wait() until game:IsLoaded()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/main/New_Text_Document.lua'))()]]
 	end
 	SaveSettings()
