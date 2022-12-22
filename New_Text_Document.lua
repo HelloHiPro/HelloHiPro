5fc9c0699d96fe5d13cbb500e9c07d1303567183
948265 <111317527+948265@users.noreply.github.com>
2022-12-22 15:10:19 -0300

    Update New_Text_Document.lua

--- New_Text_Document.lua
+++ New_Text_Document.lua
@@ -2448,6 +2448,20 @@ SettingsTab:AddToggle({
 	end   
 })
 
+SettingsTab:AddToggle({
+    Name = "Auto Execute (Click Once)",
+    Default = false,
+    Callback = function(Value)
+        _G.autoexecute = Value
+        if _G.autoexecute and autoexvalue == 0 then autoexvalue = autoexvalue + 1
+        local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport [[
+        repeat wait() until game:IsLoaded() wait(5) print("ServerHoped or rejoined")
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/main/autoex.lua'))()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/main/New_Text_Document.lua'))()]]
+        end
+    end    
+})
+
 SettingsTab:AddSlider({
 	Name = "Clock Time",
 	Min = 1,
