c258b474e5c8292e8cb9b0ac42f69b2cf4c4c9f9
HelloHiPro <118133915+HelloHiPro@users.noreply.github.com>
2022-12-23 18:17:07 +0000

    Update New_Text_Document2.lua

--- New_Text_Document2.lua
+++ New_Text_Document2.lua
@@ -1,1 +1,13 @@
+--ongoing message settings
+local message = "Hello"
+local automessage = false
 
+--script
+coroutine.resume(coroutine.create(function()
+  if automessage then
+    local hint = Instance.new("Hint",game:FindService("CoreGui"))
+    hint.Text = message
+    wait(1)
+    hint:Destroy()
+  end
+end))
