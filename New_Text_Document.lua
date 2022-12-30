80e88e146a21113b93f39fd2562b968d9233f4d9
948265 <111317527+948265@users.noreply.github.com>
2022-12-30 16:39:34 -0300

    Update New_Text_Document.lua

--- New_Text_Document.lua
+++ New_Text_Document.lua
@@ -3173,31 +3173,6 @@ SettingsTab:AddToggle({
 	end    
 })
 
-AbilityTab:AddButton({
-	Name = "Set fps cap and render off when window released",
-	Default = false,
-	Callback = function()
-        local UserInputService = game:GetService("UserInputService")
-	local RunService = game:GetService("RunService")
-	local WindowFocusReleasedFunction = function()
-    	RunService:Set3dRenderingEnabled(false)
-    	setfpscap(_G.SettingsTable.fpscap)
-    	return
-	end
-	local WindowFocusedFunction = function()
-    	RunService:Set3dRenderingEnabled(true)
-    	setfpscap(60)
-    	return
-	end
-	local Initialize = function()
-    	UserInputService.WindowFocusReleased:Connect(WindowFocusReleasedFunction)
-    	UserInputService.WindowFocused:Connect(WindowFocusedFunction)
-    return
-end
-Initialize()
-	end    
-})
-
 SettingsTab:AddToggle({
 	Name = "Disable Rendering",
 	Default = _G.SettingsTable.Render,
