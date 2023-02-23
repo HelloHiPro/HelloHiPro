d4fd41449fc6487b43afc8f5a878f50a0a8415f9
HelloHiPro <118133915+HelloHiPro@users.noreply.github.com>
2023-02-23 14:45:09 +0000

    Delete autoex.lua

--- autoex.lua
+++
@@ -1,4 +0,0 @@
-local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport [[
-repeat wait() until game:IsLoaded() wait(5) print("ServerHoped or rejoined")
-loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/main/autoex.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/HelloHiPro/HelloHiPro/main/New_Text_Document.lua'))()]]
